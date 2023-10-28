ef6268a0ebe0968ab653e3c283a5d0c0d2406af3
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2023-10-28 10:13:17 -0300

    busca

---
+++ SalesWebMVC/SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SalesRecordsController : Controller  // mesmo nome que o model - o caminho para chegar na página está no menu (_Latout.cshtml)
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
